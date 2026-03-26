45647e5ceb0fc8ba46dd1fc14d3f52a8d34af515
Ruhama Ristu <117820906+ruhamiy@users.noreply.github.com>
2026-03-26 11:31:09 +0300

    Time: 10 ms (62.30%) | Memory: 14.1 MB (92.92%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        count = Counter(nums)
+        return [item for item, freq in count.most_common(k)]
+        
