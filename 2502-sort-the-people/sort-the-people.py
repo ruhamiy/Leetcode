feef8963002aae27e2b658c1a61828e4c5b998c3
Ruhama Ristu <117820906+ruhamiy@users.noreply.github.com>
2026-04-06 11:44:48 +0300

    Time: 11 ms (45.94%) | Memory: 12.5 MB (91.34%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def sortPeople(self, names, heights):
+        """
+        :type names: List[str]
+        :type heights: List[int]
+        :rtype: List[str]
+        """
+        return [name for _, name in sorted(zip(heights, names), reverse=True)]
+
+        
