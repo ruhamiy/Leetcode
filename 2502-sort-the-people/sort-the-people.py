46a18c1f9a497d6afe7d9101e565f0d079565457
Ruhama Ristu <117820906+ruhamiy@users.noreply.github.com>
2026-04-06 12:34:58 +0300

    Time: 438 ms (16.61%) | Memory: 12.7 MB (62.37%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,3 +1,40 @@
+"""bubble sort """
+"""class Solution(object):
+    def sortPeople(self, names, heights):
+        :type names: List[str]
+        :type heights: List[int]
+        :rtype: List[str]
+    
+        n = len(names)
+        
+        for i in range(n):
+            for j in range(0, n - i - 1):
+                if heights[j] < heights[j + 1]:
+                    heights[j], heights[j + 1] = heights[j + 1], heights[j]
+                    names[j], names[j + 1] = names[j + 1], names[j]
+        
+        return names"""
+
+
+"""selection sort """
+"""class Solution(object):
+    def sortPeople(self, names, heights):
+    
+        :type names: List[str]
+        :type heights: List[int]
+        :rtype: List[str]
+    
+        n = len(names)
+        for i in range(n):
+            max_index = i
+            for j in range(i + 1, n):
+                if heights[j] > heights[max_index]:
+                    max_index = j
+            heights[i], heights[max_index] = heights[max_index], heights[i]
+            names[i], names[max_index] = names[max_index], names[i]
+        
+        return names"""
+"""insertion sort """
 class Solution(object):
     def sortPeople(self, names, heights):
         """
@@ -5,6 +42,23 @@ class Solution(object):
         :type heights: List[int]
         :rtype: List[str]
         """
-        return [name for _, name in sorted(zip(heights, names), reverse=True)]
+        n = len(names)
+        
+        # Insertion Sort based on heights
+        for i in range(1, n):
+            key_height = heights[i]
+            key_name = names[i]
+            j = i - 1
+            
+            while j >= 0 and heights[j] < key_height:
+                heights[j + 1] = heights[j]
+                names[j + 1] = names[j]
+                j -= 1
+            
+            heights[j + 1] = key_height
+            names[j + 1] = key_name
+        
+        return names
+
 
         
